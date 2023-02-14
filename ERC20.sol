b89704843ed6c0093c593f4fe0e15ac60b000cae
roythakur <125339477+roythakur@users.noreply.github.com>
2023-02-14 12:43:13 +0530

    first commit
    created variables of token

---
+++ ERC20.sol
@@ -0,0 +1,8 @@
+//SPDX-Licence-Identifier: UNLISENSED
+pragma solidity ^0.8.9;
+contracrt{
+string public name ="My Token";
+string public symbol = "MT";
+uint256 public totalSupply = 100000;
+address public owner;
+}
