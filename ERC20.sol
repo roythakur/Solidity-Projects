2bb9e11c1ce3bf0e1485ec2fdd7838769a2638f6
RAHUL SINGH <125339477+roythakur@users.noreply.github.com>
2023-02-20 19:18:20 +0530

    Update ERC20.sol

--- ERC20.sol
+++ ERC20.sol
@@ -11,3 +11,13 @@ constructor() {
 balances[msg.sender] = totalSupply;
 owner = msg.sender;
 }
+ function transfer(address to, uint256 amount) external {
+        require(balances[msg.sender] >= amount, "Not enough tokens");
+        balances[msg.sender] -= amount;
+        balances[to] += amount;
+        emit Transfer(msg.sender, to, amount);
+    }
+    function balanceOf(address account) external view returns (uint256) {
+        return balances[account];
+    }
+}
